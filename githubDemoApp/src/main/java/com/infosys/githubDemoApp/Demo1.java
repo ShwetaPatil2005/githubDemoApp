bb60c0d2b77d598b8c7c08f6247348e697c05a97
ShwetaPatil2005 <patilsweta2005@gmail.com>
2026-03-19 20:10:55 +0530

    Second version

---
+++ githubDemoApp/src/main/java/com/infosys/githubDemoApp/Demo1.java
@@ -0,0 +1,12 @@
+package com.infosys.githubDemoApp;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hii everyone!!!");
+
+	}
+
+}
--- githubDemoApp/src/main/java/com/infosys/githubDemoApp/Welcome.java
+++ githubDemoApp/src/main/java/com/infosys/githubDemoApp/Welcome.java
@@ -4,5 +4,6 @@ public class Welcome {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to github demo");
+		System.out.println("Happy new year!!");
 	}
 }
